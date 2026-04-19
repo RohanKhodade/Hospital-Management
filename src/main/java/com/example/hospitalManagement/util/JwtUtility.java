07c54a5be45e0b5ce67b611a52881269d01f0903
RohanKhodade <rohankhodade883@gmail.com>
2026-04-19 14:46:49 +0530

    docker file added and Github actions workflow

--- src/main/java/com/example/hospitalManagement/util/JwtUtility.java
+++ src/main/java/com/example/hospitalManagement/util/JwtUtility.java
@@ -3,6 +3,7 @@ package com.example.hospitalManagement.util;
 import com.example.hospitalManagement.service.implementations.UserServiceImpl;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import io.jsonwebtoken.Jwts;
@@ -17,22 +18,26 @@ public class JwtUtility {
         this.userServiceImpl=userServiceImpl;
     }
 
-    private final String SECRET="secret key to generate jwt token";
-    private final SecretKey KEY=Keys.hmacShaKeyFor(SECRET.getBytes());
-    private final long EXPIRATION_TIME=1000*60*60;
+    @Value("${jwt.secret}")
+    private String SECRET;
+    private SecretKey getKey(){
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+    @Value("${jwt.expiration}")
+    private long EXPIRATION_TIME;
 
     public String generateToken(String username){
         return Jwts.builder()
                 .subject(username)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis()+EXPIRATION_TIME))
-                .signWith(KEY)
+                .signWith(getKey())
                 .compact();
     }
 
     public String extractUsername(String token){
         Claims body=Jwts.parser()
-                .verifyWith(KEY)
+                .verifyWith(getKey())
                 .build()
                 .parseSignedClaims(token)// verifies tokens expiration time, is valid
                 .getPayload();
